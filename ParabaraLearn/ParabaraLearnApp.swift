5e137b095e8a13753b07dbf84dc6ac74f9478268
JangSSeung <tmdgh1xn@gmail.com>
2025-03-25 16:54:07 +0900

    Initial Commit

---
+++ ParabaraLearn/ParabaraLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParabaraLearnApp.swift
+//  ParabaraLearn
+//
+//  Created by 승호 on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ParabaraLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParabaraLearnTests/ParabaraLearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParabaraLearnTests.swift
+//  ParabaraLearnTests
+//
+//  Created by 승호 on 3/25/25.
+//
+
+import Testing
+@testable import ParabaraLearn
+
+struct ParabaraLearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
