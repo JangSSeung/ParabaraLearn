72677f8618a74c11111051221c1872cb47b2bffc
JangSSeung <tmdgh1xn@gmail.com>
2025-03-26 23:50:16 +0900

    #commit - ProductRegistView 생성

---
+++ ParabaraLearn/View/01_Product/ProductRegist02View.swift
@@ -0,0 +1,18 @@
+//
+//  ProductRegist01View.swift
+//  ParabaraLearn
+//
+//  Created by 승호 on 3/25/25.
+//
+
+import SwiftUI
+
+struct ProductRegist01View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductRegist01View()
+}
